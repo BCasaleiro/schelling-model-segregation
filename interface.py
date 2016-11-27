4d5b9da17f1a4bef4ff9565a7ba9b9098f89bc8a
Joao Godinho <joaogodinho_4@hotmail.com>
2016-11-27 23:53:27 +0000

    initialization of a matrix with red/blue color

--- interface.py
+++ interface.py
@@ -13,7 +13,6 @@ class Application(Frame):
         self.startB = Button(self, text='Start', command=self.start_simulation)
         self.resetB = Button(self, text='Reset', command=self.reset_simulation)
 
-
         self.redblueScroll = Scale(self, orient=HORIZONTAL, label="Red/Blue Prob")
         self.redblueScroll.set(50)
 
@@ -21,6 +20,19 @@ class Application(Frame):
         self.startB.pack({"side": "left"})
         self.resetB.pack({"side": "left"})
 
+
+        self.draw_board(30)
+
+    def draw_board(self, size):
+    	self.w = Canvas(self, width = 250, height=250)
+    	length = (self.w.winfo_reqwidth()-2)/size
+    	for i in range(size):
+    		for j in range(size):
+    			if(j%2==0):
+    				self.w.create_rectangle(i*length, j*length, (i+1)*length,(j+1)*length, fill="blue")
+    			else:
+    				self.w.create_rectangle(i*length, j*length, (i+1)*length,(j+1)*length, fill="red")
+    	self.w.pack()
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
